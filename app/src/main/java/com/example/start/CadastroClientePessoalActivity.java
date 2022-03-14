429bb2e59b35feda8fa8a0e9fbb2da8c9e9f9dd1
brunaopdejesus <bruna.oliveira.656667@gmail.com>
2022-03-14 10:12:46 -0300

    trying to commit

---
+++ app/src/main/java/com/example/start/CadastroClientePessoalActivity.java
@@ -0,0 +1,16 @@
+package com.example.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class CadastroClientePessoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_cliente_pessoal);
+
+    }
+}
