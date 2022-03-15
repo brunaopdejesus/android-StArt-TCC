580d44117b74f074d5b3dc5db651fd22be26af03
brunaopdejesus <bruna.oliveira.656667@gmail.com>
2022-03-15 11:21:05 -0300

    15-03

---
+++ app/src/main/java/com/example/start/BoasVindasCliente1Activity.java
@@ -0,0 +1,14 @@
+package com.example.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BoasVindasCliente1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boas_vindas_cliente1);
+    }
+}
