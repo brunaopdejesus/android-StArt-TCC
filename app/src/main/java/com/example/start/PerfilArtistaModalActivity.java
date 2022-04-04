29eff4f8ea86da0e773f96ec5ab0c91bca994512
Bruna Oliveira <bruna.opdejesus@gmail.com>
2022-04-04 11:15:32 -0300

    Perfil de Artista
    Alert dialog para edição e exclusão de obras pendente.

---
+++ app/src/main/java/com/example/start/PerfilArtistaModalActivity.java
@@ -0,0 +1,14 @@
+package com.example.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilArtistaModalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_artista_modal);
+    }
+}
