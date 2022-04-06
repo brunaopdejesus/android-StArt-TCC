01df2a71bb64abbf205670910620a49296f9c2cb
Bruna Oliveira <bruna.opdejesus@gmail.com>
2022-04-06 11:27:51 -0300

    Starting WelcomePager

---
+++ app/src/main/java/com/example/start/PerfilClienteObrasActivity.java
@@ -0,0 +1,14 @@
+package com.example.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilClienteObrasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_cliente_obras);
+    }
+}
