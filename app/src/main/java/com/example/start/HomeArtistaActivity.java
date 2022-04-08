bac49515edd1300e916fd41a9b2fb3c07aec3d43
Bruna Oliveira <bruna.opdejesus@gmail.com>
2022-04-08 11:25:48 -0300

    Tela home de cliente e artista finalizadas

---
+++ app/src/main/java/com/example/start/HomeArtistaActivity.java
@@ -0,0 +1,14 @@
+package com.example.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeArtistaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_artista);
+    }
+}
