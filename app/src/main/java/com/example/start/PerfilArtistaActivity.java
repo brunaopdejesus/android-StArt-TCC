84f76fe2cbb8f0c4a50cbd8bd8de9ac1f2c3a1d7
Bruna Oliveira <bruna.opdejesus@gmail.com>
2022-04-13 11:26:18 -0300

    Telas de editar e adicionar obra

--- app/src/main/java/com/example/start/PerfilArtistaActivity.java
+++ app/src/main/java/com/example/start/PerfilArtistaActivity.java
@@ -2,13 +2,45 @@ package com.example.start;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PerfilArtistaActivity extends AppCompatActivity {
+
+    private ImageView arrowBack;
+    private ImageView settings;
+    private ImageView addObra;
+    private ImageView obraArtista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_artista);
-    }
+
+        arrowBack = findViewById(R.id.image_back_perfil_artista);
+        settings = findViewById(R.id.icon_setting_artista);
+        addObra = findViewById(R.id.ic_adicionar_obra_perfil_artista);
+        obraArtista = findViewById(R.id.obra_perfil_artista);
+
+        arrowBack.setOnClickListener(view -> {
+            startActivity(new Intent(PerfilArtistaActivity.this, HomeArtistaActivity.class));
+        });
+
+        // SETTINGS
+//        settings.setOnClickListener(view -> {
+//            startActivity(new Intent(PerfilArtistaActivity.this, ));
+//        });
+
+        addObra.setOnClickListener(view -> {
+            startActivity(new Intent(PerfilArtistaActivity.this, AdicionarObraActivity.class));
+        });
+
+        obraArtista.setOnClickListener(view -> {
+            startActivity(new Intent(PerfilArtistaActivity.this, PerfilArtistaModalActivity.class));
+        });
+
+}
+
 }
--- app/src/main/java/com/example/start/PerfilArtistaModalActivity.java
+++ app/src/main/java/com/example/start/PerfilArtistaModalActivity.java
@@ -2,13 +2,37 @@ package com.example.start;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PerfilArtistaModalActivity extends AppCompatActivity {
+
+    private ImageView arrowBack;
+    private Button btnExcluir;
+    private Button btnEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_artista_modal);
+
+        arrowBack = findViewById(R.id.image_back_perfil_artista_modal);
+        btnExcluir = findViewById(R.id.btn_excluir_obra);
+        btnEditar = findViewById(R.id.btn_editar_obra);
+
+        arrowBack.setOnClickListener(view -> {
+            startActivity(new Intent(PerfilArtistaModalActivity.this, PerfilArtistaActivity.class));
+        });
+
+//        btnExcluir.setOnClickListener(view -> {
+//            startActivity(new Intent(PerfilArtistaModalActivity.this, .class));
+//        });
+//
+//        btnEditar.setOnClickListener(view -> {
+//            startActivity(new Intent(PerfilArtistaModalActivity.this, .class));
+//        });
+
     }
 }
